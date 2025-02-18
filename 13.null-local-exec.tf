9eed778f44ae53095e541c9c037e98a4cd69d3ed
Bhanuchander-kotturi <prabhasbhanu2023@gmail.com>
2025-02-18 23:15:06 +0530

    changed 13

--- 13.null-local-exec.tf
+++ 13.null-local-exec.tf
@@ -1,9 +1,9 @@
 resource "null_resource" "webservers" {
-  provisioner "local-exec" {
-    command = <<EOH
+  provisioner "remote-exec" {
+    inline = <<EOH
       sleep 10
       ansible -i invfile pvt -m ping
-    EOH
+    EOH 
   }
   depends_on = [local_file.ansible-inventory-file]
 }
